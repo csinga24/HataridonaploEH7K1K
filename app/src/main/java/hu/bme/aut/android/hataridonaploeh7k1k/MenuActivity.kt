99e374a23fd7196cddc73fdb38d72fcdfcfa2fed
Csinga24 <oszlanyi.csenge624@gmail.com>
2020-04-13 01:42:50 +0200

    menu activity

---
+++ app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/MenuActivity.kt
@@ -0,0 +1,78 @@
+package hu.bme.aut.android.hataridonaploeh7k1k
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_menu.*
+import android.view.MenuItem as MenuItem
+
+class MenuActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val firebaseUser: FirebaseUser? = com.google.firebase.auth.FirebaseAuth.getInstance().currentUser
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_calendar, R.id.nav_notes, R.id.nav_habittracker), drawerLayout
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        val headerView =  navView.getHeaderView(0);
+        val textViewName : TextView = headerView.findViewById(R.id.nav_header_textView_name);
+        textViewName.setText(firebaseUser?.displayName);
+        val textViewEmail : TextView = headerView.findViewById(R.id.nav_header_textView_email);
+        textViewEmail.setText(firebaseUser?.email);
+        val logoutButton : Button = headerView.findViewById(R.id.nav_logout)
+        logoutButton.setOnClickListener {
+            logOut()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    //override fun onBackPressed() { logOut() }
+
+    private fun logOut(): Boolean{
+        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+        return true
+    }
+}
