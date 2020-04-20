1dde1a5b076d4688296395549917de1a21af4a3c
Csinga24 <oszlanyi.csenge624@gmail.com>
2020-04-20 18:52:46 +0200

    add event creator activity and fix note creator

--- app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/ui/calendar/fragments/CreateEventActivity.kt
+++ app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/ui/calendar/fragments/CreateEventActivity.kt
@@ -1,42 +1,41 @@
-package hu.bme.aut.android.hataridonaploeh7k1k.ui.notes
+package hu.bme.aut.android.hataridonaploeh7k1k.ui.calendar.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.FirebaseDatabase
 import hu.bme.aut.android.hataridonaploeh7k1k.R
-import hu.bme.aut.android.hataridonaploeh7k1k.data.Note
+import hu.bme.aut.android.hataridonaploeh7k1k.data.Event
 import kotlinx.android.synthetic.main.activity_create_note.*
 import hu.bme.aut.android.hataridonaploeh7k1k.extension.validateNonEmpty
+import kotlinx.android.synthetic.main.activity_create_event.*
 
-class CreateNoteActivity : AppCompatActivity() {
+class CreateEventActivity : AppCompatActivity() {
 
     var user: FirebaseUser? = com.google.firebase.auth.FirebaseAuth.getInstance().currentUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_note)
+        setContentView(R.layout.activity_create_event)
 
-        button_add_new_note.setOnClickListener {sendClick()}
+        button_add_new_event.setOnClickListener {sendClick()}
     }
 
-    private fun validateForm() = note_title.validateNonEmpty() && note_desc.validateNonEmpty()
+    private fun validateForm() = event_title.validateNonEmpty() && event_desc.validateNonEmpty()
 
     private fun sendClick() {
         if (!validateForm()) {
             return
         }
-        val key = FirebaseDatabase.getInstance().reference.child("notes").push().key ?: return
-        val newNote = Note(user?.uid, note_title.text.toString(), Note.Priority.LOW, note_desc.text.toString(), null)
+        val key = FirebaseDatabase.getInstance().reference.child("events").push().key ?: return
+        val newEvent = Event(user?.uid, event_title.text.toString(),null, null, event_desc.text.toString())
         FirebaseDatabase.getInstance().reference
-            .child("notes")
+            .child("events")
             .child(key)
-            .setValue(newNote)
+            .setValue(newEvent)
             .addOnCompleteListener {
-                Toast.makeText(this, "Új jegyzet hozzáadva", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Új esemény hozzáadva", Toast.LENGTH_SHORT).show()
                 finish()
             }
     }
