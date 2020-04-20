91cfc146047122dd27ec7719d8850b7897e45d7f
Csinga24 <oszlanyi.csenge624@gmail.com>
2020-04-20 18:49:15 +0200

    change note data

--- app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/data/Note.kt
+++ app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/data/Note.kt
@@ -7,10 +7,11 @@ class Note (
     var uid: String?,
     var title: String,
     var priority: Priority,
-    var description: String
+    var description: String,
+    var imageUrl: String?
     ) {
 
-    constructor() : this(null, "", Priority.LOW, "")
+    constructor() : this(null, "", Priority.LOW, "", null)
 
     enum class Priority {
         LOW, MEDIUM, HIGH
