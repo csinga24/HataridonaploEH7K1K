1bb290287d85f347fc8de1f5cd5a39e5ab37db89
Csinga24 <oszlanyi.csenge624@gmail.com>
2020-04-21 00:15:56 +0200

    update adapter to notes recycler view

--- app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/ui/notes/adapter/NotesAdapter.kt
+++ app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/ui/notes/adapter/NotesAdapter.kt
@@ -1,14 +1,17 @@
 package hu.bme.aut.android.hataridonaploeh7k1k.ui.notes.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import hu.bme.aut.android.hataridonaploeh7k1k.R
 import hu.bme.aut.android.hataridonaploeh7k1k.data.Note
 import kotlinx.android.synthetic.main.card_note.view.*
+import com.bumptech.glide.Glide
 
 class NotesAdapter(private val context: Context?) : RecyclerView.Adapter<NotesAdapter.ViewHolder>()  {
 
@@ -17,6 +20,9 @@ class NotesAdapter(private val context: Context?) : RecyclerView.Adapter<NotesAd
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvTitle: TextView = itemView.tvTitle
         val tvDesc: TextView = itemView.tvDescription
+        val imgNote: ImageView = itemView.imgNote
+        val tvPriority: TextView = itemView.tvPriority
+        val view: View = itemView
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -32,11 +38,31 @@ class NotesAdapter(private val context: Context?) : RecyclerView.Adapter<NotesAd
         val tmpNotes = notesList[position]
         holder.tvTitle.text = tmpNotes.title
         holder.tvDesc.text = tmpNotes.description
+
+        if (tmpNotes.imageUrl.isNullOrBlank()) {
+            holder.imgNote.visibility = View.GONE
+        } else {
+            context?.let { Glide.with(it).load(tmpNotes.imageUrl).into(holder.imgNote) }
+            holder.imgNote.visibility = View.VISIBLE
+        }
+        when(tmpNotes.priority){
+            Note.Priority.LOW -> {
+                holder.tvPriority.text = "Nem Fontos"
+                holder.view.setBackgroundColor(Color.parseColor("#F1E263")) //<color name="colorLow">#F1E263</color>
+            }
+            Note.Priority.MEDIUM -> {
+                holder.tvPriority.text = "Átlagos"
+                holder.view.setBackgroundColor(Color.parseColor("#EF734C")) //<color name="colorMedium">#EF734C</color>
+            }
+            Note.Priority.HIGH -> {
+                holder.tvPriority.text = "Fontos"
+                holder.view.setBackgroundColor(Color.parseColor("#EF3232")) //<color name="colorHigh">#EF3232</color>
+            }
+        }
     }
 
     fun addNote(note: Note?){
         note ?: return
-
         notesList.add(note)
         notifyDataSetChanged()
     }
