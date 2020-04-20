6a0f90e2b97e18e3b4d97cb8678eca50f22f57f7
Csinga24 <oszlanyi.csenge624@gmail.com>
2020-04-20 18:50:05 +0200

    add a fragment to calendar view

--- app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/ui/calendar/CalendarFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import hu.bme.aut.android.hataridonaploeh7k1k.R
+import hu.bme.aut.android.hataridonaploeh7k1k.R.layout
+import hu.bme.aut.android.hataridonaploeh7k1k.ui.calendar.fragments.CalCalendarFragment
+import hu.bme.aut.android.hataridonaploeh7k1k.ui.calendar.fragments.ListCalendarFragment
 
 class CalendarFragment : Fragment(){
         
@@ -16,16 +18,30 @@ class CalendarFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view: View? = inflater.inflate(R.layout.fragment_calendar, container, false)
+        val view: View? = inflater.inflate(layout.fragment_calendar, container, false)
         view?.let { initialView(it) }
         return view
     }
 
     fun initialView(view: View) {
-        val text:TextView = view.findViewById(R.id.text_calendar_view)
+        swapFragmnet(CalCalendarFragment())
+
         val button_cal:Button = view.findViewById(R.id.button_cal)
-        button_cal.setOnClickListener { text.text = "Cal" }
+        button_cal.setOnClickListener {
+            val newFragment = CalCalendarFragment()
+            swapFragmnet(newFragment)
+        }
         val button_list:Button = view.findViewById(R.id.button_list)
-        button_list.setOnClickListener { text.text = "List" }
+        button_list.setOnClickListener {
+            val newFragment = ListCalendarFragment()
+            swapFragmnet(newFragment)
+        }
+    }
+
+    private fun swapFragmnet(newFragment: Fragment) {
+        val fragmentTransaction =
+            activity!!.supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, newFragment)
+        fragmentTransaction.commit()
     }
 }
