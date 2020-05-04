f9059367f163ec9a331875bb66063a76942eb8ae
Csinga24 <oszlanyi.csenge624@gmail.com>
2020-05-04 22:43:53 +0200

    add database key to note

--- app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/data/Note.kt
+++ app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/data/Note.kt
@@ -4,6 +4,7 @@ import com.google.firebase.auth.FirebaseUser
 
 
 class Note (
+    var key: String? = null,
     var uid: String?,
     var title: String,
     var priority: Priority,
@@ -11,7 +12,7 @@ class Note (
     var imageUrl: String?
     ) {
 
-    constructor() : this(null, "", Priority.LOW, "", null)
+    constructor() : this(null, null, "", Priority.LOW, "", null)
 
     enum class Priority {
         LOW, MEDIUM, HIGH
