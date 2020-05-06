cf111f197e894f4c573799a5831989b3ab465a3d
Csinga24 <oszlanyi.csenge624@gmail.com>
2020-05-07 00:33:32 +0200

    add showText (toast)

---
+++ app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/extension/showText.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.android.hataridonaploeh7k1k.extension
+
+import android.content.Context
+import android.widget.Toast
+
+fun String.showText(context: Context?){
+    Toast.makeText(context, this, Toast.LENGTH_LONG).show()
+}
