20e9be6bdba90450f1fd7feb6a2c545fac9dceeb
Csinga24 <oszlanyi.csenge624@gmail.com>
2020-05-18 22:43:21 +0200

    make note comparable

--- app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/data/Note.kt
+++ app/src/main/java/hu/bme/aut/android/hataridonaploeh7k1k/data/Note.kt
@@ -5,11 +5,32 @@ class Note (
     var uid: String?,
     var title: String,
     var priority: Priority,
-    var description: String,
+    var description: String?,
     var imageUrl: String?
-    ) {
+    ): Comparable<Note> {
 
-    constructor() : this(null, null, "", Priority.LOW, "", null)
+    constructor() : this(null, null, "", Priority.LOW, null, null)
+
+    override fun compareTo(other: Note): Int {
+        if(priority == other.priority) {
+            return 0
+        }
+        else if(priority == Priority.HIGH && other.priority != Priority.HIGH){
+            return -1
+        }
+        else if(priority == Priority.LOW && other.priority != Priority.LOW){
+            return 1
+        }
+        else if(priority == Priority.MEDIUM){
+            if(other.priority == Priority.LOW){
+                return -1
+
+            }else if(other.priority == Priority.HIGH){
+                return 1
+            }
+        }
+        return 0
+    }
 
     enum class Priority {
         LOW, MEDIUM, HIGH
